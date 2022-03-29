d2d2d1a58d5d5459247a3b6a796195d5cb30d4cc
USER <USER@DESKTOP-NF0PVQI>
2022-03-29 23:10:19 +0900

    네비바 2번 문제 해결

--- SpringProject3/src/main/webapp/WEB-INF/views/nav.jsp
+++ SpringProject3/src/main/webapp/WEB-INF/views/nav.jsp
@@ -29,7 +29,7 @@
 					 <!-- 추가 및 변경 끝 부분-->
 					<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
 				</ul>
-				<label id="darkmodecheck">
+				<label id="darkmodecheck" class="cbbgc">
 					<input type="checkbox" id="checkBoxId" checked>
 					<span id="moonsun">
 						<ion-icon name='sunny-outline'></ion-icon>
--- SpringProject3/src/main/webapp/resources/js/darkmode.js
+++ SpringProject3/src/main/webapp/resources/js/darkmode.js
@@ -1,4 +1,5 @@
 var darkmodeIcon = document.getElementById("moonsun");
+var chkbox = document.getElementById("darkmodecheck");
 var sky = document.getElementById("sky");
 var bodytag = document.getElementsByTagName("body");
 var navbar = document.getElementById("ftco-navbar");
@@ -12,14 +13,32 @@ $(document).ready(function(){
             $(mainimg).removeClass("maindarkimg");     
             $(navbar).removeClass("darkmode");    
             $(sky).attr('class','skybird');
+            $(chkbox).removeClass("cbbgcn");
         }else{
             darkmodeIcon.innerHTML="<ion-icon name='moon-outline'></ion-icon>"
             $(bodytag).addClass("darkmode");
             $(navbar).addClass("darkmode");
             $(mainimg).addClass("maindarkimg");
             $(sky).attr('class','nightsky');
+            $(chkbox).addClass("cbbgcn");
 
             
         }
     });
 });
+$(window).scroll(function () {
+			var height = $(document).scrollTop();
+			log(height);
+			
+	});
+
+function log(h){
+	if(h > 300){
+		$('#darkmodecheck').addClass('cbbgc');
+
+	}
+	if(h < 300){
+		$('#darkmodecheck').removeClass('cbbgc');
+		$('#darkmodecheck').removeClass('cbbgcn');
+	}
+}
